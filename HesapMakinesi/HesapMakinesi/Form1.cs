02c5b103c0834032cd2a9deda4c1516910e188df
ezgisari <esari@bib-web.com>
2023-02-21 00:19:12 +0300

    hesap makinesi masaüstü

--- HesapMakinesi/HesapMakinesi/Form1.cs
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -2,9 +2,31 @@ namespace HesapMakinesi
 {
     public partial class Form1 : Form
     {
+        public int girilenSayi = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.girilenSayi = 1;
+            label1.Text = 1.ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
